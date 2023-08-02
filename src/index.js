2c9a8ecb8cc7b77f6ee4d48dbc5e27d93a5dd5dd
kengurenokRu <nastassiadunaeva@gmail.com>
2023-08-02 17:59:00 +0300

    fix: task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    if (n<0) n = n*-1;
+  let nStr = n.toString();
+  let k = nStr.length; 
+  let number = 0;
+  let pow = 1;
+  for (let i = 0; i<k; i++)
+  {
+  number = number + parseInt(nStr[i])*pow;
+  pow = pow * 10;
+  }
+  return number;
 }
